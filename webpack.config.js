dc5f7474123753a1698bee9d283694a6c9c08eb7
liangjing1018 <1174525208@qq.com>
2017-09-30 16:26:13 +0800

    提交配置信息

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,21 @@ var webpack = require('webpack');
 var path = require('path');
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
-
-
 module.exports = {
     context: path.join(__dirname)+'/src',
     /*devtool: debug ? "inline-sourcemap" : null,*/
     //entry: "./demo/lar-gallery/galleryTemplate.js",
     //entry: "./index.js",
-    entry: {index:"./index.js",galleryTemplate:"./demo/lar-gallery/galleryTemplate.js"},
+    entry: {index:"./index.js",galleryTemplate:"./demo/pages/unit/lar-gallery/galleryTemplate.js"},
     module: {
         loaders: [
             {
                 test: /\.js?$/,
-                exclude:[/node_modules/
+                /*exclude:[/node_modules/
                     //path.resolve(__dirname, "node_modules"),
-                    //path.resolve(__dirname, "src/demo"),
-                ] ,
-                //include:[/src/component/],
+                    path.resolve(__dirname, "src/demo"),
+                ] ,*/
+                include:[path.resolve(__dirname, "src/component")],
                 loader: 'babel-loader',
                /* query: {
                     presets: ['react', 'es2015'],
@@ -27,7 +25,17 @@ module.exports = {
                 }*/
             },
             //下面是使用 ant-design 的配置文件
-            { test: /\.css$/, loader: 'style-loader!css-loader' }
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            //使用pug-html-loader
+           /* {
+                test:/\.pug$/,
+                exclude: ['/node_modules/'],
+                loader: 'pug-html-loader',
+                query: {
+                    data: {name:'test'},
+                    pretty: true
+                }
+            },*/
         ]
     },
     output: {
