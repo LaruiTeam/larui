19a0e869412237e572a6ad133dc9208b136e05e8
liangjing1018 <1174525208@qq.com>
2017-11-28 18:15:20 +0800

    完善注释

--- configTool.js
+++ configTool.js
@@ -15,23 +15,21 @@ var zipper = require("zip-local");
 this.deal=function(files,lib,libFiles){
     //var entryObject={componentAll:'./index.js',vendor:'jquery'};
     var entryObject={componentAll:'./index.js'};
-    //this.zipFile('./dist/V0.1/lib/**');
     for(var i=0;i<=files.length;i++) {
         if (i < files.length) {
             //larui研发组件
             var key=files[i].substring(files[i].lastIndexOf('/')+1,files[i].indexOf('.js'));
             entryObject[key]=files[i].replace('src/','');
         } else {
-            for (var j = 0; j <= lib.length; j++) {//第三方单独文件
+            for (var j = 0; j <= lib.length; j++) {
+                //第三方单独文件
                 if (j < lib.length) {
                     var isMin=lib[j].indexOf("min");
                     //console.log("isSinglwMin:"+isMin);
                     if(isMin<0){
                         var key="/lib/"+lib[j].substring(lib[j].lastIndexOf('/')+1,lib[j].indexOf('.js'));
                         entryObject[key]=lib[j].replace('src/','') ;
-                        //console.log("key:" + key+",pathName:"+lib[j].replace('src/',''));
                     }
-
                 } else {//第三方组件文件夹
                     travel('./src/lib/', function (pathName) {
                         pathName = pathName.replace(/\\/g, "/");
@@ -45,13 +43,9 @@ this.deal=function(files,lib,libFiles){
                             if(isMin<0 && isJs>0){  //没有压缩的js文件进行压缩
                                 var key = pathName.substring(pathName.lastIndexOf('/lib/'),pathName.indexOf(".js"));
                                 if(key.length>0){
-                                    //console.log("key:" + key+",pathName:./"+pathName.replace('src/', ''));
                                     entryObject[key] = "./"+pathName.replace('src/', '');
                                 }
                             }else{
-                                //if(isPic>-1){ //非图片信息直接复制,文件夹是否存在
-
-                                    //console.log("是否是图片："+isPic+pathName);
                                 var reDir = pathName.substring(pathName.lastIndexOf('/lib/'),pathName.length);
                                 var createPath=path.resolve(__dirname, 'dist/V0.1'+reDir);
                                 var mkdir=createPath.substring(0,createPath.lastIndexOf('\\'));
@@ -60,25 +54,13 @@ this.deal=function(files,lib,libFiles){
                                 if(!hasDir){
                                     makeDir(mkdir);
                                 }
-                                    /*if(createPath.indexOf("css")){ //css进行跟踪
+                                    /*if(createPath.indexOf("css")){ //css进行跟踪--暂时不做压缩处理
                                         console.log("css文件路径："+createPath);
                                     }*/
-
-                                   /* console.log("创建文件路径："+createPath);
-                                    fs.writeFile(createPath,"111",function(err){
-                                        if (err) {
-                                            console.log("创建失败"+err);
-                                            //throw err;
-                                        }else{
-                                            console.log("创建成功"+createPath);
-                                        }
-                                    });*/
-                                    fs.createReadStream("./"+pathName).pipe(fs.createWriteStream(createPath)).on('error', function(error){
-                                        console.log('writeStream error', error.message);
-                                    });
-                                //}
+                                fs.createReadStream("./"+pathName).pipe(fs.createWriteStream(createPath)).on('error', function(error){
+                                    console.log('writeStream error', error.message);
+                                });
                             }
-
                         }
                     });
 
@@ -95,9 +77,7 @@ function travel(dir, callback) {
         var pathname = path.join(dir, file);
         if (fs.statSync(pathname).isDirectory()) {
             travel(pathname, callback);
-            //fs.createReadStream("./src/lib/"+file).pipe(fs.createWriteStream("./dist/V0.1/lib"));
         } else {
-            //string(pathname);
             callback(pathname);
         }
     });
@@ -112,7 +92,7 @@ function find(str,cha,num){
     return x;
 }
 
-/*压缩指定路径下的一级文件夹为压缩文件，目前供下载使用*/
+/*压缩指定路径下的一级文件夹为压缩文件，目前供下载使用,具体位置 结合环境（开发还是线上）待定*/
 this.zipFile=function(folderPath){
     var zipSrcFiles = glob.sync(folderPath);
     var folders="";
@@ -137,16 +117,11 @@ this.zipFile=function(folderPath){
                 console.log("folder:"+"./dist/V0.1/lib/"+folderName+"/**");
                 zipper.sync.zip("./dist/V0.1/lib/"+folderName).compress().save("./dist/V0.1/zip/"+folderName+".zip");
             }
-
         }
-
     }
-
 }
 
 function makeDir(path){
-    //var hasDir=fs.existsSync(path);
-    //console.log("文件夹是否存在："+hasDir+"???"+createPath+"创建路径：:"+mkdir);
     var faDir=path.substring(0,path.lastIndexOf('\\'));
     var hasFaDir=fs.existsSync(faDir);
     if(!hasFaDir){
