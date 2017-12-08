8f721c90b102bba76809b0f7ce60ec959447b9c8
liangjing1018 <1174525208@qq.com>
2017-12-08 17:52:15 +0800

    调整菜单方式

--- configTool.js
+++ configTool.js
@@ -17,8 +17,12 @@ this.deal=function(files,lib,libFiles){
         if (i < files.length) {
             //larui研发组件
             var key=files[i].substring(files[i].lastIndexOf('/')+1,files[i].indexOf('.js'));
-            entryObject[key]=files[i].replace('src/','');
-            //this.componentList.push({[key]:files[i].replace('src/','')});
+            if(key.indexOf("lar")>-1){
+                entryObject[key]=files[i].replace('src/','');
+                console.log("压缩"+key);
+                //this.componentList.push({[key]:files[i].replace('src/','')});
+            }
+
         } else {
             for (var j = 0; j <= lib.length; j++) {
                 //第三方单独文件
@@ -32,15 +36,14 @@ this.deal=function(files,lib,libFiles){
                         //a[key]=lib[i].replace('src/','');
                         //this.libList.push(a);
                     }else if(isMin>0){
-                        console.log(lib[j]+"是压缩文件");
+                        //console.log(lib[j]+"是压缩文件");
                         var filePath=lib[j];
                        fs.readFile(lib[j], 'utf-8', function(err, data) {
                             if (err) {
                                 console.log("读取失败");
                             } else {
                                 var key=filePath.substring(filePath.lastIndexOf('/')+1,filePath.length);
-                                console.log(filePath+":"+"读取成功"+key);
-
+                                //console.log(filePath+":"+"读取成功"+key);
                                 var fileDestPath="./dist/V0.1/lib/"+key
                                 writeFile(fileDestPath,data)
                                 return data;
@@ -155,24 +158,26 @@ function makeDir(path){  //规范：path最后必须不是以\结尾
         fs.mkdirSync(path);
     }
 }
-this.zipFile("./dist/V0.1/lib/**/");
+//this.zipFile("./dist/V0.1/lib/**/");
 
 this.getComponents=function(){
     var comps = glob.sync('./src/component/lar-*/*.js');
     var lib = glob.sync('./src/lib/*.js');
     var libFiles = glob.sync('./src/lib/**/**');
-    for(var i=0;i<=comps.length;i++){
-        //if(comps[i].substring('lar')>-1){
-        //    var key=comps[i].substring(comps[i].lastIndexOf('/')+1,comps[i].indexOf('.js'));
-        //    //componentList.push(key+"组件");
-        //    var a={};
-        //    a[key]=files[i].replace('src/','');
-        //    this.componentList.push(a);
-        //}else{
-        //    console.log("共有研发组件"+i+"个");
-        //}
-    }
+
+    console.log(comps[0]+"iii");
+    var str=leftMenu(comps);
+
+  /*  fs.appendFile('./demo/src/pug/common/menu.pug',str,[{'flag':'}'}],function(err)
+    {if (err)  console.log("error:"+str); });
+    console.log("./demo/src/pug/common/menu.pug:"+str);*/
+    console.log("str:"+str)
+    fs.writeFile('./demo/src/pug/common/menu.pug', str, function (err) {
+        console.log(str);
+    });
 }
+
+this.getComponents();
 
 function writeFile(fileDestPath,data){
     console.log(fileDestPath+"路径");
@@ -187,4 +192,45 @@ function writeFile(fileDestPath,data){
             console.log(fileDestPath+"文件已保存");
         }
     });
+}
+
+function leftMenu(comps){
+    var componentList=[{}];
+    console.log(comps.length+"个文件");
+    var j=0;
+    for(var i=0;i<=comps.length;i++){
+        //console.log(comps[i]+"iii");
+        if(i<comps.length){
+            var key=comps[i].substring(comps[i].lastIndexOf('/')+1,comps[i].indexOf('.js'));
+            if(key.indexOf('lar')>-1){
+                componentList[j]=JSON.stringify(componentList[j]);
+                var replaceUrl=componentList[j];
+                replaceUrl=replaceUrl.replace('"baseUrl','baseUrl + "')
+                componentList[j]=replaceUrl;
+                j++;
+            }
+        }else{
+            console.log("共有研发组件"+j+"个");
+            var str='mixin leftMenu(baseUrl)\n'
+                +'      - var componentMenus = ['+componentList.toString()+']\n'
+                +'      div.componentMenus \n'
+                +'          ul\n'
+                +'              li.title \n'
+                +'                  span 组件列表 \n'
+                +'              each item in componentMenus \n'
+                +'                  li \n'
+                +'                      - var menuName=item.menuName\n'
+                +'                      a(href=item.link) #{menuName} \n'
+                +'      div.styleMenus \n'
+                +'          ul \n'
+                +'              li.title \n'
+                +'                  span 样式 \n'
+                +'              each item in componentMenus \n'
+                +'                  li \n'
+                +'                      - var menuName=item.menuName \n'
+                +'                      a(href=item.link) #{menuName} \n'
+
+            return str;
+        }
+     }
 }
