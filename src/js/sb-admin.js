c6963fffdf0da6d5a036970cb5a7b22603f73331
renkaiguang <644509407@qq.com>
2018-01-05 17:49:32 +0800

    添加了larBackTop页面

--- src/js/sb-admin.js
+++ src/js/sb-admin.js
@@ -23,23 +23,7 @@
     this.scrollTop += (delta < 0 ? 1 : -1) * 30;
     e.preventDefault();
   });
-  // Scroll to top button appear
-  $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+
   // Configure tooltips globally
   $('[data-toggle="tooltip"]').tooltip()
-  // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top)
-    }, 1000, 'easeInOutExpo');
-    event.preventDefault();
-  });
 })(jQuery); // End of use strict
